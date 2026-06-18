2262752d8028212c587a909a877265dd558b654f
JHome1 <112623883+JHome1@users.noreply.github.com>
2026-06-18 14:45:53 +0800

    Update train.py

--- Point-Transformer-pointcam-online/tool/train.py
+++ Point-Transformer-pointcam-online/tool/train.py
@@ -2,8 +2,6 @@
 # Modified from 'Point Transformer' 
 # Reference: https://github.com/POSTECH-CVLab/point-transformer
 # -------------------------------------------------------------
-import sys
-sys.dont_write_bytecode = True 
 import os
 
 import time
